a8fcb03fdd7be3c24a7b4f1dcc14029eecd60fc8
Benoît Morgan <bmorgan@laas.fr>
2015-02-10 15:01:10 +0100

    Suppression de example qui sert à rien

--- sources/example/sources.mk
+++
@@ -1,4 +0,0 @@
-# sources here
-#
-# SRC_VERILOG_$(d) += $(d)/test.v 
-# SRC_VERILOG_$(d) += $(CORES_DIR)/core/test.v 
--- sources/rules.mk
+++ sources/rules.mk
@@ -4,8 +4,6 @@ d		:= $(dir)
 
 dir	:= $(d)/cores
 include	$(dir)/rules.mk
-# dir	:= $(d)/example
-# include	$(dir)/rules.mk
 dir	:= $(d)/counter
 include	$(dir)/rules.mk
 
