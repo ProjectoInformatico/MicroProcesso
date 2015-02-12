6726119bfe0b639efc634eda7537b662097c8df6
Benoît Morgan <bmorgan@laas.fr>
2015-02-12 13:01:50 +0100

    Impact Jtag frequency issue was due to a system numeric locale misconfiguration....

--- xilinx.sh
+++ xilinx.sh
@@ -3,7 +3,10 @@ XILINX_BASE="/home/bmorgan/Xilinx"
 XILINX_VERSION="14.7"
 DISPLAY=:0
 export XIL_IMPACT_USE_LIBUSB=1
+# export LD_PRELOAD="/usr/lib64/libstdc++.so.6 ~/Xilinx/14.7/ISE_DS/common/lib/lin64/libusb-driver.so"
 export LD_PRELOAD=~/Xilinx/14.7/ISE_DS/common/lib/lin64/libusb-driver.so
 emulate sh
 source ${XILINX_BASE}/${XILINX_VERSION}/ISE_DS/settings64.sh
 emulate zsh
+# Bug in frequency
+export LC_NUMERIC=en_US.UTF-8
