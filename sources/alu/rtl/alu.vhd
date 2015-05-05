bb7f326ba82120645c5a1978c354e1d397973037
thetlk <jeremieboutoille@gmail.com>
2015-05-05 15:07:57 +0200

    Working addition

--- sources/alu/rtl/alu.vhd
+++ sources/alu/rtl/alu.vhd
@@ -4,48 +4,29 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity alu is
-port(
-    CLK : in STD_LOGIC ;
-    RST : in STD_LOGIC ;
-    A : in std_logic_vector(7 downto 0) ;
-    B : in std_logic_vector(7 downto 0) ;
-    Ctrl_Alu : in std_logic_vector(2 downto 0);
-    S : out std_logic_vector(7 downto 0) ;
-    N : out STD_LOGIC;
-    O : out STD_LOGIC;
-    Z : out STD_LOGIC;
-    C : out STD_LOGIC
+    port(
+        A : in std_logic_vector(7 downto 0) ;
+        B : in std_logic_vector(7 downto 0) ;
+        Ctrl_Alu : in std_logic_vector(2 downto 0);
+        S : out std_logic_vector(7 downto 0) ;
+        N : out STD_LOGIC;
+        O : out STD_LOGIC;
+        Z : out STD_LOGIC;
+        C : out STD_LOGIC
     );
 end entity ; -- alu
 
 architecture Behavioral of alu is
     signal Sortie : std_logic_vector(8 downto 0);
-
 begin
+
+    Sortie <= ("0" & A) when Ctrl_Alu = "000" else -- simple copy
+              ("0" & A) + B when Ctrl_Alu = "001" else "000000000"; -- addition
+
     S <= Sortie(7 downto 0);
-    O <= Sortie(8);
-    C <= '1' when Sortie(8) = '1' and Ctrl_Alu = "001" else '0';
-    N <= '1' when Sortie(8) = '1' and Ctrl_Alu = "010" else '0';
-    Z <= '1' when Sortie = X"00" else '0';
-
-    process(CLK)
-    begin
-        if rising_edge(CLK) then
-            if RST = '0' then
-                Sortie <= (others => '0');
-            else 
-                case Ctrl_Alu is
-                    when "001" => -- 1.Addition
-                        Sortie <= ("0" & A) + B;
-                    when "010" => -- 2.Soustraction
-                        Sortie <= ("0" & A) - B;
-                        
-                    when others => -- 0.Defaut S=A
-                        Sortie <= "0" & A;
-                end case;
-
-            end if;
-        end if;
-    end process;
+    Z <= '1' when Sortie(7 downto 0) = X"00" else '0';
+    O <= '1' when A(7) = B(7) and Sortie(7) /= A(7) else '0'; -- quand on change de signe en add/sub/mult des nombres de même signe
+    C <= Sortie(8);
+    N <= Sortie(7);
 
 end architecture ; -- Behavioral of alu
--- sources/alu/rtl/alu_bench.vhd
+++ sources/alu/rtl/alu_bench.vhd
@@ -1,52 +1,42 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
+library ieee;
+use ieee.std_logic_1164.all;
+
 
 entity alu_bench is 
 end entity ; -- alu_bench 
 
 architecture behavior of alu_bench is 
-  component alu 
-  port(
-    CLK : in STD_LOGIC ;
-    RST : in STD_LOGIC ;
-    A : in std_logic_vector(7 downto 0) ;
-    B : in std_logic_vector(7 downto 0) ;
-    Ctrl_Alu : in std_logic_vector(2 downto 0);
-    S : out std_logic_vector(7 downto 0) ;
-    N : out STD_LOGIC;
-    O : out STD_LOGIC;
-    Z : out STD_LOGIC;
-    C : out STD_LOGIC
-    );
-  end component;
+    component alu 
+        port(
+            A : in std_logic_vector(7 downto 0) ;
+            B : in std_logic_vector(7 downto 0) ;
+            Ctrl_Alu : in std_logic_vector(2 downto 0);
+            S : out std_logic_vector(7 downto 0) ;
+            N : out STD_LOGIC;
+            O : out STD_LOGIC;
+            Z : out STD_LOGIC;
+            C : out STD_LOGIC
+        );
+    end component;
 
-   --Inputs
-   signal CLK : STD_LOGIC := '0';
-   signal RST : STD_LOGIC := '0';
-   signal Ctrl_Alu : std_logic_vector(2 downto 0) := (0 => '0', others => '0');
-   signal A : std_logic_vector(7 downto 0) := (0 => '1', others => '0');
-   signal B : std_logic_vector(7 downto 0) := ( others => '1');
+    --Inputs
+    signal Ctrl_Alu : std_logic_vector(2 downto 0);
+    signal A : std_logic_vector(7 downto 0);
+    signal B : std_logic_vector(7 downto 0);
 
-  --Outputs
-   signal S : std_logic_vector(7 downto 0);
-   signal O : STD_LOGIC;
-   signal C : STD_LOGIC;
-   signal N : STD_LOGIC;
-   signal Z : STD_LOGIC;
- 
-   -- constants
-   constant CLK_period : time := 10 ns;
+    --Outputs
+    signal S : std_logic_vector(7 downto 0);
+    signal O : STD_LOGIC;
+    signal C : STD_LOGIC;
+    signal N : STD_LOGIC;
+    signal Z : STD_LOGIC;
+     
+    -- constants
+    constant pause : time := 50 ns;
 
 begin 
   -- Instantiate the Unit Under Test (UUT)
    uut: alu PORT MAP (
-          CLK => CLK,
-          RST => RST,
           Ctrl_Alu => Ctrl_Alu,
           A => A,
           B => B,
@@ -56,40 +46,40 @@ begin
           N => N,
           Z => Z
         );
- 
-   CLK_process : process
-   begin
-        CLK <= not CLK;
-        wait for CLK_period/2;
-   end process;
 
-   -- Stimulus process
-   stim_proc: process
-   begin    
-      -- hold reset state for 100 ns.
-      
-      wait for CLK_period*10;
-      RST <= '1';
 
-      wait for CLK_period*10;
-      A <= X"00";
+    -- Stimulus process
+    stim_proc: process
+        begin    
 
-      wait for CLK_period*10;
-      A <= X"FF";
+        -- test de la copie de A
+        wait for pause;
+            Ctrl_Alu <= "000";
+            A <= X"FF";
 
-      -- Add test
-      wait for CLK_period*10;
-      Ctrl_Alu <= "001";
-      A <= X"01";
-      B <= X"FF";
+        -- test de l'addition
+        wait for pause;
+            Ctrl_Alu <= "001";
+            A <= X"01";
+            B <= X"04";
+            -- S must be 05 and no flagz.
 
-      -- Sub test
-      wait for CLK_period*10;
-      Ctrl_Alu <= "010";   
-      A <= X"FF";
-      B <= X"FF";
+        wait for pause;
+            A <= X"FF";
+            B <= X"01";
+            -- S must be 00 and Z, C flag
 
-      wait;
-   end process;
+        wait for pause;
+            A <= X"7F";
+            B <= X"7F";
+            -- S must be FE and N, O flag
+
+        wait for pause;
+            A <= X"99";
+            B <= X"BB";
+            -- S must be 54 and C, O flag
+
+        wait for pause;
+    end process;
 
 end architecture ; -- arch
