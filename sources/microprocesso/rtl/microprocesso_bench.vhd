0a42ba67d857fddbc2f256d9ee60d973a6186a48
thetlk <jeremieboutoille@gmail.com>
2015-05-13 12:59:56 +0200

    Rewrite micro_beh

--- sources/microprocesso/rtl/microprocesso_bench.vhd
+++ sources/microprocesso/rtl/microprocesso_bench.vhd
@@ -1,89 +1,39 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   11:05:21 02/11/2015
--- Design Name:   
--- Module Name:   counter/rtl/microprocesso_bench.vhd
--- Project Name:  counter
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: system
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This microprocessobench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under microprocesso.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the microprocessobench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 library ieee ;
-    use ieee.std_logic_1164.all ;
-    use ieee.numeric_std.all ;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
-ENTITY microprocesso_bench IS
-END microprocesso_bench;
- 
-ARCHITECTURE behavior OF microprocesso_bench IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT microprocesso
-    PORT(
-         CLK : IN  std_logic;
-         RST : IN  std_logic
-        );
-    END COMPONENT;
-    
+	use ieee.std_logic_1164.all ;
+	use ieee.numeric_std.all ;
 
-   --Inputs
-   signal CLK : std_logic := '0';
-   signal RST : std_logic := '0';
+entity microprocesso_bench is
+end entity ; -- microprocesso_bench
 
-   -- Clock period definitions
-   constant CLK_period : time := 10 ns;
- 
-BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: microprocesso PORT MAP (
-          CLK => CLK,
-          RST => RST
-        );
+architecture behavior of microprocesso_bench is
 
-   -- Clock process definitions
-   CLK_process :process
-   begin
-		CLK <= '0';
-		wait for CLK_period/2;
-		CLK <= '1';
-		wait for CLK_period/2;
-   end process;
- 
+	component microprocesso
+		port(
+		    CLK : in STD_LOGIC;
+		    RST : in STD_LOGIC
+		);
+	end component;
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      -- hold reset state for 100 ns.
-      wait for 100 ns;	
-      RST <= '1';
-      wait for CLK_period*10;
-      
-      -- insert stimulus here 
+	constant period : time := 10 ns;
 
-      wait;
-   end process;
+	signal clk : std_logic := '1';
+	signal rst : std_logic := '0';
 
-END;
+begin
+
+	uut: microprocesso
+	port map(clk, rst);
+
+	clk <= not clk after period/2;
+
+	stim : process
+	begin
+		wait for 100 ns;
+
+		rst <= '1';
+		wait for period*10;
+
+		wait;
+	end process ; -- stim
+
+end architecture ; -- behavior
