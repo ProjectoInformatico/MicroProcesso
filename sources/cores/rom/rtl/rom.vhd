1e3b3784bc285446695127619b45b765204fc2ce
David LIVET <david.livet@orange.fr>
2015-05-21 11:29:36 +0200

    Test basic program

--- sources/cores/rom/rtl/rom.vhd
+++ sources/cores/rom/rtl/rom.vhd
@@ -20,7 +20,19 @@ architecture behavioral of rom is
 
 	subtype word is unsigned(WORD_SIZE-1 downto 0);
 	type word_list is array(0 to WORD_COUNT-1) of word;
-	signal words : word_list := (0 => x"08010100", others => x"00000000");
+	signal words : word_list := (
+		0 => x"06001000", -- AFC R0 0x10
+		1 => x"00000000", -- NOP
+		2 => x"00000000", -- NOP
+		3 => x"00000000", -- NOP
+		4 => x"08000000", -- STORE R0 @0
+		5 => x"06010100", -- APC R1 0x01
+		6 => x"00000000", -- NOP
+		7 => x"00000000", -- NOP
+		8 => x"00000000", -- NOP
+		9 => x"01000001", -- ADD R0 R0 R1
+
+		others => x"00000000");
 
 begin
 
